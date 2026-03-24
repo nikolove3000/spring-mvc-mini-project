4d909fe5cf7c0898c82774842963da5599cdeefa
Nikolove3000 <mynamelathanh193@gmail.com>
2026-03-24 11:15:13 +0700

    add spring mvc java config

--- src/main/java/com/example/miniproject/config/WebMvcConfig.java
+++ src/main/java/com/example/miniproject/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.example.miniproject.config;
 
+import com.example.miniproject.interceptor.LoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -19,5 +21,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor()).addPathPatterns("/students/**");
     }
 }
